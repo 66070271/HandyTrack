6527c6d024bf6b56f5050b4a7c404dfbb32e5d67
pleum <66070263@kmitl.ac.th>
2024-03-30 22:29:29 +0700

    wasd

--- src/net/handytrack/database/DBmanipulation.java
+++ src/net/handytrack/database/DBmanipulation.java
@@ -1,5 +1,18 @@
 package net.handytrack.database;
 
 public class DBmanipulation extends DBconnect{
+    public DBmanipulation(){
+        super();
+    }
+    public DBmanipulation(String url){
+        super(url);
+    }
+    private static DBmanipulation instance;
+    public static DBmanipulation getInstance(){
+        if(instance == null){
+            instance = new DBmanipulation();
+        }
+        return instance;
+    }
 
 }
