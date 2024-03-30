1fe01ef4e26d566e5b1a0d81559037c65b80c99c
marttpq <66070271@kmitl.ac.th>
2024-03-31 03:15:11 +0700

    z

---
+++ src/net/handytrack/HANDYTRACKMAIN/User.java
@@ -0,0 +1,20 @@
+package net.handytrack.HANDYTRACKMAIN;
+
+public class User {
+    private String name;
+    private String surename;
+    private String email;
+    private String tel;
+    public String getName(){
+        return this.name;
+    }
+    public String getSurename(){
+        return this.surename;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public String getTel(){
+        return this.tel;
+    }
+}
